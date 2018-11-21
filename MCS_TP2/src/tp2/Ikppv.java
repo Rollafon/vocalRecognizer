45fac212ac9bcb790ea3e322e83e7c2a4bb203fe
romai <romai@DESKTOP-BUE8JVH>
2018-11-22 00:29:44 +0100

    Add code

--- MCS_TP2/src/tp2/Ikppv.java
+++ MCS_TP2/src/tp2/Ikppv.java
@@ -1,5 +1,12 @@
 package tp2;
 
 public interface Ikppv {
-
+	
+	/**
+	 * Search the nearest label
+	 * @param k
+	 * @return
+	 */
+	public String searchLabel(int k);
+	
 }
--- MCS_TP2/src/tp2/Kppv.java
+++ MCS_TP2/src/tp2/Kppv.java
@@ -1,12 +1,32 @@
 package tp2;
-import org.apache.commons.math3.linear.RealMatrix;
 
 public class Kppv implements Ikppv {
-	private RealMatrix data;
-	private RealMatrix label;
-	int i;
-	public Kppv(RealMatrix data, RealMatrix label) {
+	private IRecord[] data; // Database
+	private String[] label;	// Labels of each record of the database (same sequence)
+	
+	private int[] test; // TODO : type of expressions
+	private int k;
+
+	public Kppv(IRecord[] data, String[] label) {
 		this.data = data;
 		this.label = label;
+	}
+	
+	/**
+	 * Evaluate the distances between each point of the data base and the given test
+	 * @return
+	 */
+	private double[] evalDistances() {
+		double[] res = new double[data.length];
+		
+		for (int i = 0; i < data.length; ++i) {
+			
+		}
+		
+		return res;
+	}
+	
+	public String searchLabel(int k) {
+		
 	}
 }
