618ec819dd9c45cc5a28ee5abb260d1ba793b8dc
romai <romai@DESKTOP-BUE8JVH>
2018-11-21 23:31:18 +0100

    Test n

--- MCS_TP2/src/tp2/Kppv.java
+++ MCS_TP2/src/tp2/Kppv.java
@@ -4,8 +4,7 @@ import org.apache.commons.math3.linear.RealMatrix;
 public class Kppv implements Ikppv {
 	private RealMatrix data;
 	private RealMatrix label;
-	int test;
-	
+	int i;
 	public Kppv(RealMatrix data, RealMatrix label) {
 		this.data = data;
 		this.label = label;
