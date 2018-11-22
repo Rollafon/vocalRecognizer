bf986d24c2100d07e39e77067aa9c567dfe3e9b9
romai <romai@DESKTOP-BUE8JVH>
2018-11-22 01:06:12 +0100

    Begin distance calculation

--- MCS_TP2/src/tp2/Kppv.java
+++ MCS_TP2/src/tp2/Kppv.java
@@ -1,32 +1,40 @@
 package tp2;
 
+import org.apache.commons.math3.linear.RealMatrix;
+
+import tp1.myMFCCdistance;
+
 public class Kppv implements Ikppv {
-	private IRecord[] data; // Database
+	private IDataBase datas; // Database
 	private String[] label;	// Labels of each record of the database (same sequence)
 	
-	private int[] test; // TODO : type of expressions
+	private IDataBase tests; // TODO : type of expressions
 	private int k;
+	
+	myMFCCdistance distCalc = new myMFCCdistance();
 
-	public Kppv(IRecord[] data, String[] label) {
-		this.data = data;
+	public Kppv(IDataBase datas, String[] label) {
+		this.datas = datas;
 		this.label = label;
 	}
 	
 	/**
 	 * Evaluate the distances between each point of the data base and the given test
-	 * @return
+	 * @return the array of distances
 	 */
-	private double[] evalDistances() {
-		double[] res = new double[data.length];
-		
-		for (int i = 0; i < data.length; ++i) {
-			
+	private RealMatrix evalDistances() {
+		// TODO
+		double[][] res = new double[datas.getBase().getRowDimension()][tests.getBase().getRowDimension()];
+		for (int j = 0; j < res.length; ++j) {
+			for (int i = 0; i < res.length; ++i) {
+				res[i][j] = distCalc.distance(datas.getBase().getRow(i), datas.getBase().getRow(j));
+			}
 		}
 		
 		return res;
 	}
 	
-	public String searchLabel(int k) {
+	public String searchLabel(IDataBase tests, int k) {
 		
 	}
 }
